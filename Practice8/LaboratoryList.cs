8799bf5f376e3f5aa18f311c1ebf7685e10a5ddb
Алексей <alexeikraichik@yandex.ru>
2026-03-13 13:56:26 +0300

    Добавьте файлы проекта.

---
+++ Practice8/LaboratoryList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice8
+{
+    public class Sample
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string GetDate { get; set; }
+        public string Status { get; set; }
+        public string Result { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, {Type}, Дата: {GetDate}, Статус: {Status}, Результат: {Result}";
+        }
+    }
+
+    public class LabList : List<Sample> 
+    { 
+        public void Add(int Id, string Type, string GetDate, string Status, string Result)
+        {
+            this.Add(new Sample { Id = Id, Type = Type, GetDate = GetDate, Status = Status, Result = Result });
+        }
+
+        public void IndAdd(int index, int Id, string Type, string GetDate, string Status, string Result)
+        {
+            this.Insert(index, new Sample { Id = Id, Type = Type, GetDate = GetDate,Status = Status, Result = Result });
+        }
+
+        public void RemoveLast()
+        {
+            this.RemoveAt(this.Count - 1);
+        }
+
+        public void IndDelete(int index)
+        {
+            this.RemoveAt(index);
+        }
+
+        public int ElemCounts()
+        {
+            return this.Count;
+        }
+
+        public void IndChange(int index, int Id, string Type, string GetDate, string Status, string Result)
+        {
+            if (index >= 0 && index < this.Count)
+            {
+                this[index].Id = Id;
+                this[index].Type = Type;
+                this[index].GetDate = GetDate;
+                this[index].Status = Status;
+                this[index].Result = Result;
+                Console.WriteLine("Изменение элемента!");
+            }
+            else
+            {
+                Console.WriteLine("Нет такого индекса");
+            }
+        }
+
+        public void ShowAll()
+        {
+            foreach (var item in this)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void IndShow(int index)
+        {
+            Console.WriteLine(this[index]);
+        }
+    }
+}
---
+++ Practice8/Program.cs
@@ -0,0 +1,102 @@
+﻿using Practice8;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        LabList sample = new LabList();
+
+        do
+        {
+            Console.WriteLine("1 - Добавить элемент в конец");
+            Console.WriteLine("2 - Добавить элемент по индексу");
+            Console.WriteLine("3 - Изменить элемент по индексу");
+            Console.WriteLine("4 - Удалить последний элемент");
+            Console.WriteLine("5 - Удалить элемент по индексу");
+            Console.WriteLine("6 - Получить количество элементов");
+            Console.WriteLine("7 - Вывести список");
+            Console.WriteLine("8 - Вывести элемент по индексу");
+            Console.WriteLine("9 - Выход");
+
+            try
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Введите Id");
+                        int Id = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите Тип");
+                        string type = Console.ReadLine();
+                        Console.WriteLine("Введите Дату");
+                        string date = Console.ReadLine();
+                        Console.WriteLine("Введите Статус");
+                        string status = Console.ReadLine();
+                        Console.WriteLine("Введите Результат");
+                        string result = Console.ReadLine();
+                        sample.Add(Id, type, date, status, result);
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите индекс куда вставляете новый элемент");
+                        int index = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите Id");
+                        int Id2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите Тип");
+                        string type2 = Console.ReadLine();
+                        Console.WriteLine("Введите Дату");
+                        string date2 = Console.ReadLine();
+                        Console.WriteLine("Введите Статус");
+                        string status2 = Console.ReadLine();
+                        Console.WriteLine("Введите Результат");
+                        string result2 = Console.ReadLine();
+                        sample.IndAdd(index, Id2, type2, date2, status2, result2);
+                        break;
+                    case 3:
+                        Console.WriteLine("Введите индекс изменяемого элемента");
+                        int index2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите Id");
+                        int Id3 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите Тип");
+                        string type3 = Console.ReadLine();
+                        Console.WriteLine("Введите Дату");
+                        string date3 = Console.ReadLine();
+                        Console.WriteLine("Введите Статус");
+                        string status3 = Console.ReadLine();
+                        Console.WriteLine("Введите Результат");
+                        string result3 = Console.ReadLine();
+                        sample.IndChange(index2, Id3, type3, date3, status3, result3);
+                        break;
+                    case 4:
+                        sample.RemoveLast();
+                        break;
+                    case 5:
+                        Console.WriteLine("Введите индекс элемента который хотите удалить");
+                        int index3 = Convert.ToInt32(Console.ReadLine());
+                        sample.IndDelete(index3);
+                        break;
+                    case 6:
+                        int count = sample.ElemCounts();
+                        Console.WriteLine($"Количество элементов {count}");
+                        break;
+                    case 7:
+                        sample.ShowAll();
+                        break;
+                    case 8:
+                        Console.WriteLine("Введите индекс элемента который хотите увидеть");
+                        int index4 = Convert.ToInt32(Console.ReadLine());
+                        sample.IndShow(index4);
+                        break;
+                    case 9:
+                        
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Вы ничего не выбрали");
+            }
+        }
+        while (true);
+    }
+}
